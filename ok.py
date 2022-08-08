685678a8a91c364f44b00655e3159b9dc87fe159
josephinenakk <josephine.nakka@yahoo.com>
2022-08-07 22:53:41 -0500

    first commit

---
+++ ok.py
@@ -0,0 +1,13 @@
+from pickletools import read_int4
+
+
+name = "paul"
+name1 = 'john'
+print ("paul")
+print (name[1])
+a = 0.5
+print (a)
+marriagestatus = True
+marriagestatus = False 
+print(type (name))
+print(type (a))
