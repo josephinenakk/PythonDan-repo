508cb34f88e459b01060f8490dde3545d5434bbc
josephinenakk <josephine.nakka@yahoo.com>
2022-08-08 13:47:50 -0500

    first commit

---
+++ data_type.py
@@ -0,0 +1,23 @@
+"""
+HTML and CSS.
+Python.
+Java.
+JavaScript.
+Swift.
+C++
+C#
+R
+"""
+name = "john"
+name = 'john'
+age = 24
+# middle name =""
+height_in_centimeters = 56
+height_in_centimeters = 56.7
+marital_status =True
+weight_in_kilograms = 80
+
+
+print(name,age,height_in_centimeters,marital_status,weight_in_kilograms)
+print(name[0])
+print(name[3])
---
+++ day2-p.py
@@ -0,0 +1,45 @@
+# 1.How Python Stores the values in its memory?
+"""
+Variables are nothing but reserved memory locations to store values. 
+That is when you create a variable you reserve some space in memory.
+Python optimizes memory utilization by allocating the same object reference 
+to a new variable if the object already exists with the same value.
+ """
+
+# 2. What is a variable Why we need one ?
+""" 
+A Python variable is a symbolic name that is a reference or pointer to an object.
+Once an object is assigned to a variable, you can refer to the object by that name.
+"""
+# 3. How to declare a variable ?
+"""
+a = "gfinejoe"
+b = 3
+"""
+
+# 4. comments in python.
+"""
+1. #singleline comment
+""" 
+""" 2. multilinecomment """
+''' 3. multilinecomment  '''
+""" 
+
+"""
+
+# 5. python syntax.
+"""
+print ("name") #print statement example
+"""
+# 6. indentation in Python.
+"""
+Indentation refers to the spaces at the beginning of a code line. 
+Where in other programming languages the indentation in code is for readability only, 
+the indentation in Python is very important. Python uses indentation to indicate a block of code.
+"""
+
+# 7. GitHub
+  #https://github.com/josephinenakk/PythonDan-repo.git
+
+ 
+# 8. Create individual  branch
