5aa74530fb6e8cfbd2f88415dcdc478fc4cdf027
josephinenakk <josephine.nakka@yahoo.com>
2022-08-22 12:11:48 -0500

    added dictionary and result_dictionary files

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 #######    Python Dictionary Methods     #########
+print("Python Dictionary Methods ******************************************")
 """
 Method				Description
 1.	copy()		Returns a copy of the dictionary.
@@ -14,7 +15,6 @@ Method				Description
 11.	clear()		Removes all the elements from the dictionary.
 """
 
-
 dictinary_var = {
                  "first_name":"joe",
                  "age":46,
@@ -24,17 +24,17 @@ dictinary_var = {
                  "pets":"two",
                  "no_of_childern":2
                  }
-print(dictinary_var,"\n")
+print("\n line 27- printing dictinary_var= \n", dictinary_var)
 
 #1.copy()
 x=dictinary_var.copy()
-print("1.Using copy method to print dictinary_var",x,'\n')
+print("\n line 31- 1.Using copy method to print dictinary_var= \n",x)
 
 #2.fromkeys() for each first list item  it will take it as key and  assign all list items of second as value .
 x=('first_name', 'age', 'height', 'weight')
 y=('joe')
 OneWayFoCreateDictonary = dict.fromkeys(x, y)
-print("2.Using formkey creating Dictonary=",OneWayFoCreateDictonary)
+print("\n line 37- 2.Using formkey creating Dictonary= \n",OneWayFoCreateDictonary)
 
 """2.Using formkey creating Dictonary=
  {'first_name': 'joe', 
@@ -44,36 +44,36 @@ print("2.Using formkey creating Dictonary=",OneWayFoCreateDictonary)
 
 #3.get()	
 y=dictinary_var.get("marital_status")
-print("3.Using get method finding the value of marital_status: ",y,'\n')
+print("\n line 47- 3.Using get method finding the value of marital_status= \n ",y)
 
 #4.items()
 z = dictinary_var.items()
-print("4.Using items method: ",z,'\n')
+print("\n line 51- 4.Using items method: \n",z)
 
 #5.keys()	
 k = dictinary_var.keys()
-print("5.Using keys method to get keys of dictinary:",k,'\n')
+print("\n line 55- 5.Using keys method to get keys of dictinary= \n",k)
 
 #6.pop()
 x=dictinary_var.pop("pets")
 print(x)
-print("6.Using pop method removing pets: ",dictinary_var,'\n')
+print("\n line 60- 6.Using pop method removing pets: \n",dictinary_var)
 
 #7.popitem()
 dictinary_var.popitem()
-print("7.Using pop method removing pets: ",dictinary_var,'\n')
+print("\n line 64- 7.Using pop method removing pets= \n ",dictinary_var)
 
 #8.setdefault() , it doesn't change the value of key but if key does't exit it appends the key value.
 x=dictinary_var.setdefault("first_name","Ammulu")
-print("8.1 From setdefault method with old key 'first_name'= ",x,'\n') 
+print("\n line 68- 8.1 From setdefault method with old key 'first_name'= \n",x) 
 x=dictinary_var.setdefault("nick_name","Ammulu")
-print("8.2 From setdefault method with new key 'nick_name'= ",x,'\n')
+print("\n line 70- 8.2 From setdefault method with new key 'nick_name'= \n ",x)
 print( dictinary_var)
 
 #9.update()
 dictinary_var.update({"color":"Green"})
-print("9 From update method= ", dictinary_var,'\n')
+print("\n line 75- 9 From update method= \n", dictinary_var)
 
 #10.values()
 z=dictinary_var.values()
-print("10. from values method printing all values=",z)
+print("\n line 79- 10. from values method printing all values= \n",z,"\n")
