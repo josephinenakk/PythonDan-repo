caab83fb297b9ffbfefa5057fcefb73b4198b85e
josephinenakk <josephine.nakka@yahoo.com>
2022-08-30 14:37:42 -0500

    added loop_assignment4.py

---
+++ loop_assignment4.py
@@ -0,0 +1,87 @@
+x = [] # empty list
+b= 1
+y={}
+i=1
+while i<=50: #loop 50 times
+  j=1
+  while j<=3: #loop 3 times
+    
+    if (j%2 == 0 ):
+       y.update({i:i*j}) 
+    #else:
+        #y.update({i:i*j}) 
+    j+=1
+    if(i==50 and j==3):
+        print(y)
+  i+=1
+  #print ("\n",x)
+x.append(y)
+#print("\n line-28",x[0])
+#print(x)
+
+y={}
+m=1
+while m<=50: #loop 50 times
+  n=1
+  while n<=3: #loop 3 times
+    
+    if (n%2 == 0 ):
+
+       y.update({m:(m+50)*n}) 
+    
+    #else:
+        #y.update({i:i*j}) 
+    n+=1
+    if(m==50 and n==3):
+        print(y)
+  m+=1
+  #print ("\n",x)
+x.append(y)
+#print("\n line -39", x)
+
+y={}
+j=1
+while j<=50: #loop 50 times
+  k=1
+  while k<=3: #loop 3 times
+    
+    if (k%2 == 0 ):
+
+       y.update({j:(j+100)*k}) 
+    
+    #else:
+        #y.update({i:i*j}) 
+    k+=1
+    if(j==50 and k==3):
+        print(y)
+  j+=1
+  #print ("\n",x)
+x.append(y)
+
+print("\n\n\n\n", x)
+
+
+
+
+
+
+
+
+"""
+for i in range(0,3): # getting one by one 0 ,1,2
+    y = {}           #empty dect
+    x.append(y)      #[{},{},{}]
+    if (i < 1):      # i=0
+       index= 1        
+    else:
+        index = i*100+1 #i= 1,2 (101, 201)
+    for a in range(index,100+index): #(101,201)  (201,301)
+        if (a%2 != 0 ):
+            continue
+        y.update({b:a}) 
+        b+=1
+    x[i] = y
+#print (x)
+
+"""
+
