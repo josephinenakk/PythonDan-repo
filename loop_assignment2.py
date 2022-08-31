f9b9c9a34353efeda5ac3e3a3533224e607b8089
josephinenakk <josephine.nakka@yahoo.com>
2022-08-31 15:19:58 -0500

    using if and elif

--- loop_assignment2.py
+++ loop_assignment2.py
@@ -1,4 +1,5 @@
-#1. Write program to get result below
+#1. Write program to get result below 
+#using for and if
 # [ 
     # {1:2,2:4,3:6 ........ 49:98},
     # {1:102,2:104,3:106 ........ 49:198},
@@ -9,25 +10,59 @@ index1=1
 index2=1
 index3=1
 list1=[{},{},{}]
-for i in range(1,100):
+
+for i in range(1,101):
     if( i%2 != 0):
         continue
     y.update({index1:i})
     index1 +=1
 list1[0].update(y)
-#print("\n",x)
-for i in range(100,200):
+#list1.append(y)
+print("\n",list1)
+
+for i in range(100,201):
     if(i%2 != 0):
         continue
     y.update({index2:i})
     index2 +=1
 list1[1].update(y)
-#print("\n",list1)
-for i in range(200,300):
+print("\n",list1)
+
+
+for i in range(200,301):
     if(i%2 != 0):
         continue
     y.update({index3:i})
     index3 +=1
 list1[2].update(y)
 print(list1)
+
+################################################################################3
+
+# 2. Write program to get result below
+#using for ,if ,elif
+# [ 
+    # {1:2,2:4,3:6 ........ 49:98},
+    # {1:102,2:104,3:106 ........ 49:198},
+    # {1:202,2:204,3:206 ........ 49:298}
+# ]
+y={}
+index1=1
+index2=1
+index3=1
+list1=[{},{},{}]
+for i in range(1,301):
+    if( i<101 and i%2 == 0):
+        y.update({index1:i})
+        index1 +=1
+        list1[0].update(y)
+    elif(i>101 and i<201 and i%2 == 0):
+        y.update({index2:i})
+        index2 +=1
+        list1[1].update(y)
+    elif(i<301 and i%2 == 0):
+        y.update({index3:i})
+        index3 +=1
+        list1[2].update(y)
+print("\n",list1)
 
