b300eded30a93c161b86755a7517aec2a3d58544
josephinenakk <josephine.nakka@yahoo.com>
2022-09-12 08:57:18 -0500

    bank_account3.py update

--- bank_account3.py
+++ bank_account3.py
@@ -1,6 +1,21 @@
+"""Python Learnings Bank
+    Please enter account number - 8943  # This account number should be 4 digit
+    Please enter valid account number
+    Account doesnt exists!! please try again
+    Please enter your pin - 45 # pin number should always be 2 digit
+       Welcom Mr Mark / Ms Leena
+       please select from following options
+            1. withdrawl
+            2. balance display
+            if 2 -> Your account has 34$
+            if 1->  please enter amount you want to withdraw - 35
+            if withdrawl amount is in the range please update the balance in the dictionary 
+            else display insufficient funds message
+        you want to continue 1 or 2 
+        to exit press 3"""
+        
 people = {1234: {'name': 'Mark', 'balance': 27, 'Gender': 'Male','pin':45},
           4567: {'name': 'Marie', 'balance': 22, 'Gender': 'Female','pin':32}}
-1
 def gettingAccountNumber():
     global x
     x=input("Account number:")
